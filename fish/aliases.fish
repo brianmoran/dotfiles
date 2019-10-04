ac671d3d9c02dba8792584058df283bbd0275a49
Brian Moran <bmoran@urbn.com>
2019-10-04 14:29:03 -0400

    Add git alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,6 +1,9 @@
 #!/usr/local/bin/fish
 
 alias g "git"
+alias gsl "g stash list"
+alias gsa "g stash apply"
+
 alias dps "docker ps --format \"table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}\""
 
 alias tmux-attach "tmux attach-session -t tmux-base; or tmux new-session -s tmux-base"
